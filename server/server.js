2f559112e207ccf6bd15c2c6d58e5643620a918a
Silencepriest <destroyer.rus@gmail.com>
2020-09-29 00:53:06 +0300

    midnight save

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import express from 'express'
 import path from 'path'
 import cors from 'cors'
@@ -9,6 +10,8 @@ import React from 'react'
 import cookieParser from 'cookie-parser'
 import config from './config'
 import Html from '../client/html'
+
+const { readFile, stat } = require('fs').promises;
 
 const Root = () => ''
 
@@ -26,6 +29,56 @@ try {
   console.log(' run yarn build:prod to enable ssr')
 }
 
+const isFileExists = async (category) => {
+  const result = await stat(`${__dirname}/tasks/${category}.json`).then(() => true).catch(() => false)
+  return result
+}
+
+const getData = async (category) => {
+  const fileExists = await isFileExists(category)
+  if (!fileExists) {
+    return false
+  }
+  const content = await readFile(`${__dirname}/tasks/${category}.json`, { codepage: 'utf8' })
+  const parsedContent = JSON.parse(content)
+  // eslint-disable-next-line dot-notation
+  const parsedContentNotDeleted = parsedContent.filter((item) => !item['_isDeleted'])
+  return parsedContentNotDeleted
+}
+
+const parseContent = (content) => {
+  const parsedContent = content.map((item) => {
+    return {
+      taskId: item.taskId,
+      title: item.title,
+      status: item.status
+    }
+  })
+  return parsedContent
+}
+
+const DAY = 1000 * 60 * 60 * 24
+const WEEK = 7 * 1000 * 60 * 60 * 24
+const MONTH = 30 * 1000 * 60 * 60 * 24
+
+const filterData = (content, how) => {
+  const now = +new Date()
+  switch (how) {
+    case 'day': {
+      return content.filter((item) => item._createdAt + DAY > now)
+    }
+    case 'week': {
+      return content.filter((item) => item._createdAt + WEEK > now)
+    }
+    case 'month': {
+      return content.filter((item) => item._createdAt + MONTH > now)
+    }
+    default: {
+      return { status: 'error' }
+    }
+  }
+}
+
 let connections = []
 
 const port = process.env.PORT || 8090
@@ -40,6 +93,22 @@ const middleware = [
 ]
 
 middleware.forEach((it) => server.use(it))
+
+server.get('/api/v1/tasks/:category', async (req, res) => {
+  const { category } = req.params
+  const data = await getData(category)
+  if (!data) res.json({ status: 'error' })
+  res.json(parseContent(data))
+  return true
+})
+
+server.get('/api/v1/tasks/:category/:timestamp', async (req, res) => {
+  const { category, timestamp } = req.params
+  const data = await getData(category)
+  if (!data) res.json({ status: 'error' })
+  res.json(parseContent(filterData(data, timestamp)))
+  return true
+})
 
 server.use('/api/', (req, res) => {
   res.status(404)
